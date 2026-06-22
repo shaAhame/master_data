a1a1442999e7b1ae6d75ae5103c56593ca855dd7
Shakeeb Ahamed <152055117+shaAhame@users.noreply.github.com>
2026-06-22 15:42:06 +0530

    Update branch sheet URLs in config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Branch sheet URLs and names
 BRANCHES = [
-    {"url": "https://docs.google.com/spreadsheets/d/1HMhBeRVO8Z8JbFsOb2CQPwKQ9eqZLdTI8-lZc2hKgsU", "name": "PRIME"},
-    {"url": "https://docs.google.com/spreadsheets/d/1NmIlTzEDK6jTP_RtQUF3Nro-5G_fyYbw6t6QYraDhKQ", "name": "LIBERTY"},
-    {"url": "https://docs.google.com/spreadsheets/d/17BkV297wMpZQDKq7KeGDT1C5jGGVbPSAW2U_7eBhRys", "name": "MARINO"}
+    {"url": "https://docs.google.com/spreadsheets/d/14swyI3Yrwl2LoW90uS8_hEVEz9WgtumpUVPInsyrjkA/edit?gid=0#gid=0", "name": "PRIME"},
+    {"url": "https://docs.google.com/spreadsheets/d/1ShC9atBDrn15heNo4kwvlW_NT0T9lr2Xc5R5NlaIi2Q/edit?gid=0#gid=0", "name": "LIBERTY"},
+    {"url": "https://docs.google.com/spreadsheets/d/12JEn7u9W9YctvnTNfFv7xqw5U-ZiqCjKZ2RsEz_PcGA/edit?gid=0#gid=0", "name": "MARINO"}
 ]
